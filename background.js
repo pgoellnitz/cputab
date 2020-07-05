b248136e6a64cfac2ad93b77a4f357587d31d477
Philippe Göllnitz <philippe.goellnitz@gmail.com>
2020-07-05 14:35:25 +0200

    no popup

--- background.js
+++ background.js
@@ -1,25 +1,42 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({showCpu: true}, function() {});
+    handleListener();
 });
 
-chrome.processes.onUpdated.addListener(function(update) {
-  //console.log(update);
+function updateCpuUsage(update) {
+  for (var key in update) {
+    const cpu = update[key].cpu;
+    const tabId = update[key].tasks[0].tabId;
+    if (tabId) {
+      chrome.tabs.get(tabId, function(tab) {
+        const title = tab.title.split("% ", 2).pop()
+        chrome.tabs.executeScript(
+          tabId,
+          { code:'document.title = "' + Math.round(cpu) + '% ' + title + '"'},
+          _=>{ chrome.runtime.lastError }
+        );
+      });
+    };
+  };
+};
+
+function handleListener() {
   chrome.storage.sync.get('showCpu', function(data) {
     if (data.showCpu) {
-      for (var key in update) {
-        const cpu = update[key].cpu;
-        const tabId = update[key].tasks[0].tabId;
-        if (tabId) {
-          chrome.tabs.get(tabId, function(tab) {
-            const title = tab.title.split("% ", 2).pop()
-            chrome.tabs.executeScript(
-              tabId,
-              { code:'document.title = "' + Math.round(cpu) + '% ' + title + '"'},
-              _=>{ chrome.runtime.lastError }
-            );
-          })
-        }
-      }
-    }
+      chrome.processes.onUpdated.addListener(updateCpuUsage);
+      chrome.browserAction.setBadgeText({text: 'on'});
+      chrome.browserAction.setBadgeBackgroundColor({color: 'blue'});
+    } else {
+      chrome.processes.onUpdated.removeListener(updateCpuUsage);
+      chrome.browserAction.setBadgeText({text: 'off'});
+      chrome.browserAction.setBadgeBackgroundColor({color: 'black'});
+    };
   });
+};
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.storage.sync.get('showCpu', function(data) {
+    chrome.storage.sync.set({ showCpu: !data.showCpu}, function() {});
+  });
+  handleListener();
 });
